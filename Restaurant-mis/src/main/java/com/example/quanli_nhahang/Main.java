917553832a8b96b8fb354de159b0de6b9dea57ef
Phạm Nhật Tân <phamnhattan0820@gmail.com>
2026-03-28 13:21:59 +0700

    remake 1
    cập nhật 28-3

--- Restaurant-mis/src/main/java/com/example/quanli_nhahang/Main.java
+++ Restaurant-mis/src/main/java/com/example/quanli_nhahang/Main.java
@@ -1,4 +1,4 @@
-package RestaurantManagement;
+package com.example.quanli_nhahang;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
